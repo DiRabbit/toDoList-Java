f08a493bced5a7093e72502b860e184d0dd985a6
Francisco Coelho <arthur.coelhoosorio23@gmail.com>
2024-08-21 20:32:06 -0300

    Update na classe Quest

--- src/entities/Quest.java
+++ src/entities/Quest.java
@@ -1,5 +1,40 @@
 package entities;
 
 public class Quest {
-    
+    private String nome;
+    private boolean concluida;
+    private int id;
+
+    public Quest(String nome) {
+        setNome(nome);
+        setConcluida(false);
+        gerarId();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+     public int getId() {
+         return id;
+     }
+
+     public void concluir() {
+        setConcluida(true);
+        System.out.println("A Task " + getNome() + " cujo id é: " + getId() + " foi concluída com sucesso!");
+    }
+
+    public void gerarId() {
+        double randomNumber = Math.random() * 5;
+        int id = (int)randomNumber;
+    }
+
 }
